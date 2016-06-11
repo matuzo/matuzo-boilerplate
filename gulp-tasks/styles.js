962c3a19808f8bfa035d15799fb08376b3d6278d
matuzo <manuel@matuzo.at>
2016-06-11 15:36:52 +0200

    added sourcemaps

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    sourcemaps = require('gulp-sourcemaps');
 /**
  * Compile less to CSS
  */
 
 gulp.task('styles', function() {
     gulp.src('app/assets/styles/styles.less')
+        .pipe(sourcemaps.init())
         .pipe(less())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/assets/styles/'))
 });
